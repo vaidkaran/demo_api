c5785aeeed8f8ec95ad588ed54ba79efc71b897a
Karan Vaid <vaidkaran89@gmail.com>
2024-07-23 14:19:41 +0530

    added root route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8082
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the demo-api');
+})
+
 app.get('/test', (req, res) => {
   res.json({success: "true"});
 })
